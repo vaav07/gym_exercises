550636f9bfde89aa7ba3f4a722ea8711a60e9bd7
Vaibhav <v071997@gmail.com>
2022-07-06 21:42:27 +0530

    Created navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Box } from '@mui/material';
-import Navbar from './components/Navbar';
-import ExerciseDetail from './pages/ExerciseDetail';
-import Home from './pages/Home';
-import Footer from './components/Footer';
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Box width="400px" sx={{ width: {xl: '1488px'}}} m="auto">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
       <Footer />
     </Box>
